0159271f42a136d4513675bfa92dfd3737818063
Juanjo Sierra <jjsierra103@gmail.com>
2022-02-06 20:18:04 +0100

    Includes support for Dark/Light themes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import PaletteDisplay from '../PaletteDisplay/PaletteDisplay';
 
 function App() {
+
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  const changeTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
   return (
-    <div className="app">
-      <h1 className="title">JJSrra.github.io</h1>
-      <PaletteDisplay></PaletteDisplay>
+    <div className={'theme ' + (darkTheme ? 'theme-dark' : 'theme-light')}>
+      <div className="app">
+        <header>
+          <h1 className="title">JJSrra.github.io</h1>
+          <button className="change-theme-button" onClick={changeTheme}><i className={'fa-moon ' + (darkTheme ? 'fas' : 'far')}/></button>
+        </header>
+        <PaletteDisplay></PaletteDisplay>
+      </div>
     </div>
   );
 }
