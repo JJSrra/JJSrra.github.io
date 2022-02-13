19e05552b0332bb4df45186baf259e19905d3872
Juanjo Sierra <jjsierra103@gmail.com>
2022-02-13 18:48:55 +0100

    Adds box-shadow to header if scrolled

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
+
 function Header(props) {
+  
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScrolled(window.scrollY > 1);
+    });
+  }, []);
 
   return (
-    <header>
+    <header className={(scrolled ? "scrolled" : "")}>
         <h1 className="title">{props.title}</h1>
-        <button className={'change-theme-button ' + (props.darkTheme ? 'dark-theme-active' : '')} onClick={props.changeTheme}>
-            <i className={'fa-moon ' + (props.darkTheme ? 'fas' : 'far')}/>
+        <button className={"change-theme-button " + (props.darkTheme ? "dark-theme-active" : "")} onClick={props.changeTheme}>
+            <i className={"fa-moon " + (props.darkTheme ? "fas" : "far")}/>
         </button>
     </header>
   );
